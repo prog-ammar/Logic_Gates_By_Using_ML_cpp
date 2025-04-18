1d72d503b4cc72b5282556f42d62a2daaf065836
prog-ammar <ammarofc2005@gmail.com>
2025-04-18 22:17:54 +0500

    Functions Decalrations of Header file

--- matrix.cpp
+++ matrix.cpp
@@ -1,10 +1,9 @@
 #include "matrix.h"
+#include<iostream>
 
 using namespace std;
 
-
-
-Matrix::Matrix(int rows,int cols)
+Matrix::Matrix(int rows=0,int cols=0)
 {
     this->rows=rows;
     this->cols=cols;
@@ -30,7 +29,7 @@ void Matrix::set_fill(int n)
 {
     for(int i=0;i<rows;i++)
     {
-        for(int j=0;i<cols;j++)
+        for(int j=0;j<cols;j++)
         {
             m[i][j]=n;
         }
@@ -38,7 +37,7 @@ void Matrix::set_fill(int n)
 }
 
 
-ostream& operator<<(ostream& output,Matrix matrix)
+ostream& operator<<(ostream& output,const Matrix& matrix)
 {
     for(int i=0;i<matrix.rows;i++)
     {
@@ -48,6 +47,22 @@ ostream& operator<<(ostream& output,Matrix matrix)
         }
         output<<"\n";
     }
-    output<<"\n";
     return output;
+}
+
+Matrix operator+(const Matrix& m1,const Matrix& m2)
+{
+    if(m1.rows==m2.rows && m1.cols==m2.cols)
+    {
+        Matrix temp(m1.rows,m2.cols);
+        for(int i=0;i<temp.rows;i++)
+        {
+            for(int j=0;j<temp.cols;j++)
+            {
+                temp.m[i][j]=m1.m[i][j]+m2.m[i][j];
+            }
+        }
+        return temp;
+    }
+    return Matrix();
 }
