1a3dd3375315174358a584fdd0b4c354d43717eb
prog-ammar <ammarofc2005@gmail.com>
2025-04-19 09:17:56 +0500

    updated

--- matrix.cpp
+++ matrix.cpp
@@ -7,20 +7,43 @@ Matrix::Matrix(int rows=0,int cols=0)
 {
     this->rows=rows;
     this->cols=cols;
-    m=new float*[this->rows];
+    m=new double*[this->rows];
     for(int i=0;i<this->rows;i++)
     {
-       m[i]=new float[this->cols];
+        m[i]=new double[this->cols];
     }
 }
 
-void Matrix::init_rand()
+Matrix::Matrix(int rows,int cols,double* ptr)
+{
+    this->rows=rows;
+    this->cols=cols;
+    m=new double*[this->rows];
+    for(int i=0;i<this->rows;i++)
+    {
+        m[i]=new double[this->cols];
+    }
+    if(ptr!=nullptr)
+    {
+        int max_size=this->rows*this->cols;
+        int l=0;
+        for(int i=0;i<this->rows;i++)
+        {
+            for(int j=0;j<this->cols;j++)
+            {
+                m[i][j]=ptr[l++];
+            }
+        }
+    }
+}
+
+void Matrix::init_rand(int low,int high)
 {
     for(int i=0;i<rows;i++)
     {
         for(int j=0;j<cols;j++)
         {
-              m[i][j]=float(rand())/RAND_MAX;
+              m[i][j]=double((float(rand())/RAND_MAX)*(high-low)+low);
         }
     }
 }
@@ -41,11 +64,12 @@ ostream& operator<<(ostream& output,const Matrix& matrix)
 {
     for(int i=0;i<matrix.rows;i++)
     {
+        output<<"\n[ ";
         for(int j=0;j<matrix.cols;j++)
         {
              output<<matrix.m[i][j]<<" ";
         }
-        output<<"\n";
+        output<<"]\n";
     }
     return output;
 }
@@ -72,12 +96,19 @@ Matrix operator*(const Matrix& m1,const Matrix& m2)
    if(m1.cols==m2.rows)
    {
     Matrix temp(m1.rows,m2.cols);
-    for(int i=0;i<m1.rows;i++)
+    temp.set_fill(0);
+    for(int i=0;i<temp.rows;i++)
     {
-        for(int j=0;j<m2.cols;j++)
+        for(int j=0;j<temp.cols;j++)
         {
-            temp.m[i][j]=m1.m[i][j]+m2.m[j][i];
+            for(int k=0;k<m2.cols;k++)
+            {
+                temp.m[i][j]+=m1.m[i][k]*m2.m[k][i];
+            }
         }
+        
     }
+    return temp;
    }
+   return Matrix();
 }
