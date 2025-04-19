dfa6cf3a4cff220c31992997ae0d99e04462b3fe
prog-ammar <ammarofc2005@gmail.com>
2025-04-19 07:55:50 +0500

    added

--- matrix.h
+++ matrix.h
@@ -11,4 +11,5 @@ class Matrix
     void set_fill(int n);
     friend ostream& operator<<(ostream& output,const Matrix& matrix);
     friend Matrix operator+(const Matrix& m1,const Matrix& m2);
+    friend Matrix operator*(const Matrix& m1,const Matrix& m2);
 };
