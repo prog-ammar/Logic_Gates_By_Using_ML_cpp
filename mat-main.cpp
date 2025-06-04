264b1cc8a8df57f6ca46956f07fd14da49b94b8a
prog-ammar <ammarofc2005@gmail.com>
2025-06-04 21:11:50 +0500

    cost working

--- mat-main.cpp
+++ mat-main.cpp
@@ -5,23 +5,9 @@ using namespace std;
 
 int main()
 {
-  Matrix w1(2,2);
-  Matrix b1(1,2);
-  Matrix w2(2,1);
-  Matrix b2(1,1);
-
-  w1.init_rand(0,1);
-  w2.init_rand(0,1);
-  b1.init_rand(0,1);
-  b2.init_rand(0,1);
-
-  w1.sigmoid();
-  w2.sigmoid();
-  b1.sigmoid();
-  b2.sigmoid();
-
-  cout<<w1;
-  cout<<b1;
-  cout<<w2;
-  cout<<b2;
+  double input[]={0,0,0,1,1,0,1,1};
+  double output[]={0,1,1,0};
+  Xor xr;
+  NeuralNetwork nn(2,Matrix(4,2,input),Matrix(4,1,output));
+  cout<<nn.cost(xr);
 }
