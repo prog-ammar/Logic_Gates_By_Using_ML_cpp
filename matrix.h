a7fe32cd3f92cee7a2af7f8a3d5d8ae11b3ded3c
prog-ammar <ammarofc2005@gmail.com>
2025-06-04 21:03:36 +0500

    Updated

--- matrix.h
+++ matrix.h
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+
 class Matrix
 {
     private:
@@ -10,12 +11,42 @@ class Matrix
     int cols;
 
     public:
+    Matrix();
     Matrix(int rows,int cols);
-    Matrix(int rows,int cols,double** ptr);
+    Matrix(const Matrix& other);
+    Matrix(int rows,int cols,double p[]);
     void init_rand(int low,int high);
     void set_fill(int n);
+    int get_rows() const;
+    int get_cols() const;
+    Matrix get_row(int i);
     void sigmoid();
+    double matrix_at(int i,int j);
     friend ostream& operator<<(ostream& output,const Matrix& matrix);
     friend Matrix operator+(const Matrix& m1,const Matrix& m2);
     friend Matrix operator*(const Matrix& m1,const Matrix& m2);
+    Matrix& operator=(const Matrix& other);
+    ~Matrix();
+};
+
+class Xor
+{
+    public:
+    Matrix a[3];
+    Matrix w[2];
+    Matrix b[2];
+    Xor();
+};
+
+class NeuralNetwork
+{
+    private:
+    int layers;
+    Matrix* input;
+    Matrix* output;
+
+    public:
+    NeuralNetwork(int layers,Matrix input,Matrix output);
+    float cost(Xor& xr);
+    ~NeuralNetwork();
 };
