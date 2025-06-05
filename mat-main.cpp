8e28960342e2e3d0e6137712546252eb2ce84f0a
prog-ammar <ammarofc2005@gmail.com>
2025-06-05 10:17:03 +0500

    Working

--- mat-main.cpp
+++ mat-main.cpp
@@ -9,5 +9,8 @@ int main()
   double output[]={0,1,1,0};
   Xor xr;
   NeuralNetwork nn(2,Matrix(4,2,input),Matrix(4,1,output));
-  cout<<nn.cost(xr);
+  nn.train(xr);
+  Matrix s=nn.get_output(xr);
+  cout<<"\nResult : ";
+  cout<<s;
 }
